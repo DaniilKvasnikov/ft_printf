148552ec2de42e89eb55f479e11be6861d549990
Rachael Gyles <rgyles@e2r2p3.21-school.ru>
2019-01-06 17:58:59 +0300

    partical version ft_d - flag 0; edit in ft_putput for flag 0

--- src/ft_d.c
+++ src/ft_d.c
@@ -6,54 +6,74 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/27 19:19:02 by rrhaenys          #+#    #+#             */
-/*   Updated: 2018/12/27 19:19:09 by rrhaenys         ###   ########.fr       */
+/*   Updated: 2019/01/06 17:52:25 by rgyles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rgyles.h"
 #include <stdio.h>
 
-int		ft_d(t_spec *elem, va_list ap)
+static int		ft_d_zerro(t_spec *elem, va_list ap)
 {
-	char *str;
-	int size;
 	int n;
-	char znak;
+	int size;
+	char sign;
+	char *str;
 
-	znak = 1;
+	size = 0;
+	sign = 1;
 	if ((n = va_arg(ap, int)) < 0)
-		znak = -1;
-	str = ft_itoa(n * znak);
-	size = ft_strlen(str);
-	if (elem->flag.plus > 0 || znak < 0)
+		sign = -1;
+	str = ft_itoa(n * sign);
+	if (elem->flag.plus > 0 || sign < 0)
 	{
-		size++;
-		if (znak > 0)
+		if (sign > 0)
 			ft_putchar('+');
 		else
 			ft_putchar('-');
+		size++;
 	}
-	else if (n > 0 && elem->flag.space > 0)
+	else if (n > 0 && elem->flag.space > 0)	
 	{
 		size++;
 		ft_putchar(' ');
 	}
-	while (size < elem->width && !elem->flag.minus && elem->flag.zerro)
+	size = ft_output(elem, str, ft_strlen(str) + size);
+	free(str);
+	return (size);
+}
+
+int		ft_d(t_spec *elem, va_list ap)
+{
+	int n;
+	int size;
+	char sign;
+	char *tmp;
+	char *str;
+
+	if (elem->flag.zerro == 1)
+		return (ft_d_zerro(elem, ap));
+	size = 0;
+	sign = 1;
+	if ((n = va_arg(ap, int)) < 0)
+		sign = -1;
+	str = ft_itoa(n * sign);
+	if (elem->flag.plus == 1 || sign == -1)
 	{
-		size++;
-		ft_putchar('0');
+		tmp = str;
+		if (sign > 0)
+			str = ft_strjoin("+", str);
+		else
+			str = ft_strjoin("-", str);
+		free(tmp);
 	}
-	while (size < elem->width && !elem->flag.minus)
+	else if (n > 0 && elem->flag.space ==1)	
 	{
-		size++;
-		ft_putchar(' ');
+		tmp = str;
+		str = ft_strjoin(" ", str);
+		free(tmp);
 	}
-	ft_putstr(str);
-	while (size < elem->width)
-	{
-		size++;
-		ft_putchar(' ');
-	}
+	size = ft_output(elem, str, ft_strlen(str) + size);
 	free(str);
 	return (size);
 }
