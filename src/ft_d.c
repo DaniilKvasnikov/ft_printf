e18d906cb53ed4392f0708e72ce4e39fa01395b6
Rachael Gyles <rgyles@e2r2p3.21-school.ru>
2019-01-08 19:55:04 +0300

    outsourced allocator in ft_d for norm

--- src/ft_d.c
+++ src/ft_d.c
@@ -6,35 +6,37 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/27 19:19:02 by rrhaenys          #+#    #+#             */
-/*   Updated: 2019/01/08 19:28:50 by rgyles           ###   ########.fr       */
+/*   Updated: 2019/01/08 19:53:52 by rgyles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rgyles.h"
 
+static char	*allocator(t_spec *elem, long long int n)
+{
+	if (elem->length.l == 1)
+		return (ft_itoa_lli((long int)n, elem));
+	else if (elem->length.j == 1)
+		return (ft_itoa_lli((intmax_t)n, elem));
+	else if (elem->length.h == 1)
+		return (ft_itoa_lli((short int)n, elem));
+	else if (elem->length.h == 2)
+		return (ft_itoa_lli((signed char)n, elem));
+	else if (elem->length.l == 2)
+		return (ft_itoa_lli(n, elem));
+	else
+		return (ft_itoa_lli((int)n, elem));
+}
+
 int			ft_d(t_spec *elem, va_list ap)
 {
-	long long int n;
-	int size;
-	char *str;
-	char *tmp;
+	int				size;
+	char			*str;
 
-	n = va_arg(ap, long long int);
-	if (elem->length.l == 1)
-		str = ft_itoa_lli((long int)n, elem);
-	else if (elem->length.j == 1)
-		str = ft_itoa_lli((intmax_t)n, elem);
-	else if (elem->length.h == 1)
-		str = ft_itoa_lli((short int)n, elem);
-	else if (elem->length.h == 2)
-		str = ft_itoa_lli((signed char)n, elem);
-	else if (elem->length.l == 2)
-		str = ft_itoa_lli(n, elem);
-	else
-		str = ft_itoa_lli((int)n, elem);
+	str = allocator(elem, va_arg(ap, long long int));
 	if (elem->precision > 0)
 		elem->flag.zerro = 0;
-	if (n == 0 && elem->precision == 0)
+	if (str[0] == '0' && elem->precision == 0)
 		size = ft_output(elem, "\0", 0);
 	else if (elem->flag.plus != 0 || elem->flag.space != 0)
 		size = ft_output(elem, str, ft_strlen(str) + 1);
