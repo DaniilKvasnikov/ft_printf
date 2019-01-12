d6b61d7f5516c20872456e41877fd3184bb51244
Red alyn of the rosewood Rhaenys targaryen <rrhaenys@e2r3p5.21-school.ru>
2019-01-12 15:38:54 +0300

    ft_p

--- src/ft_p.c
+++ src/ft_p.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_p.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgyles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 14:39:49 by rgyles            #+#    #+#             */
-/*   Updated: 2019/01/08 17:57:18 by rgyles           ###   ########.fr       */
+/*   Updated: 2019/01/12 15:36:00 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int		ft_p(t_spec *elem, va_list ap)
 
 	if ((p = ft_rebase((long long)va_arg(ap, void*), 16)) == NULL)
 		return (-1);
+	elem->flag.sharp = 1;
 	size = ft_output(elem, p, ft_strlen(p) + 2);
 	free(p);
 	return (size);
