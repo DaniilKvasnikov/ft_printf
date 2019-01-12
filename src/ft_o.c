a20bf8e000fc5e4b771d306a9b65bb032ad9c9a7
Red alyn of the rosewood Rhaenys targaryen <rrhaenys@e2r3p5.21-school.ru>
2019-01-12 22:06:39 +0300

    ft_o

--- src/ft_o.c
+++ src/ft_o.c
@@ -6,7 +6,7 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 14:43:52 by rgyles            #+#    #+#             */
-/*   Updated: 2019/01/12 21:12:34 by rgyles           ###   ########.fr       */
+/*   Updated: 2019/01/12 22:06:06 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,38 @@ static long long int	get_num(t_spec *elem, va_list ap)
 	return ((int)va_arg(ap, int));
 }
 
+char	*ft_o_minus(char *str)
+{
+	int		index;
+	int		j;
+	char	c;
+	char	*res;
+	char	*temp;
+
+	if (str == NULL || (res = ft_strnew(21)) == NULL)
+		return (NULL);
+	temp = "01234567";
+	ft_memset(res, '7', 21);
+	index = 21 - ft_strlen(str);
+	j = 0;
+	while (index < 21)
+	{
+		c = str[j++];
+		if (c >= '0' && c <= '7')
+			c = c - '0';
+		res[index] = temp[7 - c] + (index == 20);
+		index++;
+	}
+	free(str);
+	return (res);
+}
+
 int						ft_o(t_spec *elem, va_list ap)
 {
 	long long int	num;
 	char			*par;
 	int				size;
-	char			znak;
+	int				znak;
 
 	num = get_num(elem, ap);
 	znak = 1;
@@ -51,6 +77,8 @@ int						ft_o(t_spec *elem, va_list ap)
 		size = 1;
 	else
 		size = 0;
+	if (znak < 0)
+		par = ft_o_minus(par);
 	if (elem->precision != -1 && par[0] == '0')
 		size = ft_output(elem, "\0", size);
 	else
